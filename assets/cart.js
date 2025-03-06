e7dcb6237881123896a6cb6a9815d5e21b826748
Guanghui Zeng <zengguanghui@zerozero.cn>
2025-03-06 19:40:58 +0800

    log log

--- assets/cart.js
+++ assets/cart.js
@@ -29,6 +29,7 @@ class CartItems extends HTMLElement {
 
   connectedCallback() {
     this.cartUpdateUnsubscriber = subscribe(PUB_SUB_EVENTS.cartUpdate, (event) => {
+      console.log('cartUpdateUnsubscriber', event);
       if (event.source === 'cart-items') {
         return;
       }
@@ -56,6 +57,7 @@ class CartItems extends HTMLElement {
   }
 
   validateQuantity(event) {
+    console.log('validateQuantity', event);
     const inputValue = parseInt(event.target.value);
     const index = event.target.dataset.index;
     let message = '';
@@ -88,6 +90,7 @@ class CartItems extends HTMLElement {
   }
 
   onCartUpdate() {
+    console.log('onCartUpdate', this.tagName);
     if (this.tagName === 'CART-DRAWER-ITEMS') {
       return fetch(`${routes.cart_url}?section_id=cart-drawer`)
         .then((response) => response.text())
@@ -182,7 +185,8 @@ class CartItems extends HTMLElement {
 
           this.getSectionsToRender().forEach((section) => {
             const elementToReplace =
-              document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+              document.getElementById(section.id).querySelector(section.selector) ||
+              document.getElementById(section.id);
             elementToReplace.innerHTML = this.getSectionInnerHTML(
               parsedState.sections[section.section],
               section.selector
@@ -214,6 +218,7 @@ class CartItems extends HTMLElement {
 
         CartPerformance.measureFromEvent(`${eventTarget}:user-action`, event);
 
+        console.log('parsedState', parsedState);
         publish(PUB_SUB_EVENTS.cartUpdate, { source: 'cart-items', cartData: parsedState, variantId: variantId });
       })
       .catch(() => {
@@ -284,8 +289,9 @@ if (!customElements.get('cart-note')) {
           'input',
           debounce((event) => {
             const body = JSON.stringify({ note: event.target.value });
-            fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } })
-              .then(() => CartPerformance.measureFromEvent('note-update:user-action', event));
+            fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } }).then(() =>
+              CartPerformance.measureFromEvent('note-update:user-action', event)
+            );
           }, ON_CHANGE_DEBOUNCE_TIMER)
         );
       }
